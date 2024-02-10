8fd524c6a899002b3bc6e95e5b868ade816e5aa3
Samarth Vohra <officialdevelopersamarth@gmail.com>
2024-02-10 13:31:43 +0530

    songs

---
+++ Lecture-24/music-player/app.js
@@ -0,0 +1,22 @@
+let playBtn = document.getElementById("play-btn");
+let progress = document.getElementById("progress");
+let songList = document.getElementById("song-list");
+
+let songs = [
+  {
+    id: 1,
+    name: "song1",
+  },
+  {
+    id: 2,
+    name: "song2",
+  },
+  {
+    id: 3,
+    name: "song3",
+  },
+  {
+    id: 4,
+    name: "song4",
+  },
+];
