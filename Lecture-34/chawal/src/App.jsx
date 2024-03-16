168102b3253791ff1cafd7ff6dd14b29e5623791
Samarth Vohra <officialdevelopersamarth@gmail.com>
2024-03-16 13:23:00 +0530

    lecture 34 codes

---
+++ Lecture-34/chawal/src/App.jsx
@@ -0,0 +1,14 @@
+import Person from "./Components/Person";
+
+function App() {
+  return (
+    <div>
+      <h1>mai hu App.jsx</h1>
+      <Person />
+      <Person />
+      <Person />
+    </div>
+  );
+}
+
+export default App;
---
+++ Lecture-34/chawal/src/Components/Person.jsx
@@ -0,0 +1,12 @@
+function Person() {
+  return (
+    <div style={{ border: "2px solid red", backgroundColor: "lightcoral" }}>
+      <h1>Name: samarth vohra </h1>
+      <h2>Age: 29 </h2>
+      <h2>Fav color: black </h2>
+      <h2>isCommitted: bohat saari </h2>
+    </div>
+  );
+}
+
+export default Person;
